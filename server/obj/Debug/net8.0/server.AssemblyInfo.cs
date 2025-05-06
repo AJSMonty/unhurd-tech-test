c84070c68780d34d102361d79243d93156b4902b
AJ Montgomery <aj.monty@gmail.com>
2025-05-06 22:15:49 +0100

    remove appsettings

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05a1dbac49530d83f0bd5bf90d4c0232cf07ebe3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ebdecd1dcfaa9650f75ec4590e5335f58529fae")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
